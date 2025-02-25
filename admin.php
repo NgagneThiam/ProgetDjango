9091c73fc499b8f7c22fbffa041a15a32dbf4b57
NgagneThiam <tngagne933@gmail.com>
2025-02-25 13:00:58 +0000

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,29 @@
+<!-- =======  Section entete ======= -->
+<?php require_once("view/sections/admin/entete.php")   ?>
+
+
+  <!-- ======= Menu Haut ======= -->
+  <?php require_once("view/sections/admin/menuHaut.php")   ?>
+
+
+  <!-- ======= Menu Gauche ======= -->
+  <?php require_once("view/sections/admin/menuGauche.php")   ?>
+
+
+
+  <main id="main" class="main">
+
+
+<!-- ======= contenu ======= -->
+<?php require_once("view/sections/admin/contenu.php")   ?>
+
+  </main>
+
+  <!-- ======= Footer ======= -->
+  <?php require_once("view/sections/admin/footer.php")   ?>
+
+
+ <!-- ======= script ======= -->
+ <?php require_once("view/sections/admin/script.php")   ?>
+
+  
---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Ninestars Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="public/templates/templateVitrine/assets/img/favicon.png" rel="icon">
+  <link href="public/templates/templateVitrine/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="public/templates/templateVitrine/assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="public/templates/templateVitrine/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="public/templates/templateVitrine/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="public/templates/templateVitrine/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="public/templates/templateVitrine/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="public/templates/templateVitrine/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="public/templates/templateVitrine/assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Ninestars - v4.7.0
+  * Template URL: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- =======section Menu ======= -->
+    <?php require_once("view/sections/vitrine/menu.php")?> 
+
+ 
+  <!-- =======  Section Baniere ======= -->
+  <?php require_once("view/sections/vitrine/baniere.php")?>
+   
+
+  
+  <main id="main">
+
+    <!-- =======  Section Apropos ======= -->
+    <?php require_once("view/sections/vitrine/apropos.php")?>
+    
+   
+
+    <!-- =======  Section services ======= -->
+    <?php require_once("view/sections/vitrine/services.php")?>
+    
+
+    <!-- =======  Section Realisation ======= -->
+    <?php require_once("view/sections/vitrine/realisation.php")?>
+    
+    <!-- =======  Section F.A.Q ======= -->
+
+    <?php require_once("view/sections/vitrine/faq.php")?>
+    
+
+    <!-- =======  Section Equipe ======= -->
+    <?php require_once("view/sections/vitrine/equipe.php")?>
+    
+
+    <!-- =======  Section Partanaire======= -->
+    <?php require_once("view/sections/vitrine/partanere.php")?>
+    
+
+    <!-- ======= Section Contact ======= -->
+    <?php require_once("view/sections/vitrine/contact.php")?>
+    
+
+  </main><!-- End #main -->
+
+  <!-- =======section Footer ======= -->
+  <?php require_once("view/sections/vitrine/foter.php")?>
+  
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="public/templates/templateVitrine/assets/vendor/aos/aos.js"></script>
+  <script src="public/templates/templateVitrine/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="public/templates/templateVitrine/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="public/templates/templateVitrine/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="public/templates/templateVitrine/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="public/templates/templateVitrine/assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="public/templates/templateVitrine/assets/js/main.js"></script>
+
+</body>
+
+</html>
